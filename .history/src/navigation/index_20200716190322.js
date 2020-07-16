8f82f624c0424f64fffd828ebc9b61de42613bce
Alexandre Shafii <alexandre.shafii@gmail.com>
2020-07-16 19:03:44 -0400

    ed

---
+++ .history/src/navigation/index_20200716190322.js
@@ -0,0 +1,22 @@
+// imported this to make icons appear in published app
+import { registerRootComponent } from "expo";
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import Routes from "./Routes";
+import { AuthProvider } from "./AuthProvider";
+
+/**
+ * Wrap all providers here
+ */
+
+export default function Providers() {
+  return (
+    <PaperProvider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </PaperProvider>
+  );
+}
+// makes icons appear in published app
+registerRootComponent(Routes);
